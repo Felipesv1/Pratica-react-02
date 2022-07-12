4d788ec5fbb899178ceb0b3fd1a87cf12e2c9dd5
Felipe Pereira <104787971+Felipesv1@users.noreply.github.com>
2022-07-12 15:48:59 -0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class app extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+          "Se você tem um problema que não consengue resolver,Existe um problema
+          mais facil que você poder resolver; encontre-o" George polya
+        </p>
+      </div>
+    );
+  }
+}
